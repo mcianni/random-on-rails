9a288991e41985a03f7df388c3a7019afc9f1003
mike <mcianni@gmail.com>
2015-01-05 02:09:04 -0500

    Use a basic person model. Random seed data.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  require 'objspace'
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    ids = Rails.cache.fetch :people_ids do
+      Person.pluck(:id)
+    end
+    @record_index_size = ObjectSpace.memsize_of(ids)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+data = []
+time = Time.now
+conn = ActiveRecord::Base.connection
+
+Person.delete_all
+
+500000.times do 
+  data << "('#{time}', '#{time}')"
+end
+
+sql = "INSERT INTO people(created_at, updated_at) VALUES #{data.join(', ')}"
+
+4.times do
+  conn.execute sql
+end
