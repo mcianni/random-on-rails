ce056b36f62f9069758487c70e24a965b552aeb8
mcianni <mcianni@gmail.com>
2015-01-14 15:51:46 -0500

    use redis as the cache store

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Unicorn as the app server
 gem 'unicorn'
 gem 'faker'
+gem 'redis-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- test/performance/random_by_sql_test.rb
+++ test/performance/random_by_sql_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 require 'rails/performance_test_help'
 
-class RandomBySql < ActionDispatch::PerformanceTest
+class RandomBySqlTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
   # self.profile_options = { runs: 5, metrics: [:wall_time, :memory],
   #                          output: 'tmp/performance', formats: [:flat] }
